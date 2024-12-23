85bfe86c4c658a90c2233c48de7ec793a275d7ba
quisranti <israelquisbert.dev@gmail.com>
2024-12-23 01:06:44 -0400

    Mumbling completed by isra

--- Mumbling.js
+++ Mumbling.js
@@ -8,3 +8,17 @@
 // The parameter of accum is a string which includes only letters from a..z and A..Z.
 
 // CODE WARS EXCERCISE LINK -> https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/javascript
+
+function accum(s) {
+    let str_arr = s.split("");
+    let res = [];
+  for (let i = 0; i < str_arr.length; i++){
+    let word = str_arr[i].repeat(i+1);
+    let firstCapital = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    res.push(firstCapital);
+  }
+  let final_result = res.join("-");
+  return final_result;
+}
+
+console.log(accum("RqaEzty"));
